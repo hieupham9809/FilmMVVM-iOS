7fbb8e6b345ad84db01fd87bf669e1a806b2c46d
hieu pham <hieupham9809@gmail.com>
2020-04-13 10:05:05 +0700

    Initial Commit

---
+++ FilmMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FilmMVVM
+//
+//  Created by HieuPM on 4/13/20.
+//  Copyright © 2020 HieuPM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
